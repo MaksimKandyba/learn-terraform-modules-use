f5f8ffa0f78a24dcf7c9c847c0b8894f88027aab
kandyba <m.kandyba@comparus.de>
2023-11-15 17:00:16 +0100

    fly has dropped maintaining the provider, thus it's unfeasible to deploy all this

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,20 @@ output "app_name" {
 output "url" {
   value = module.vaultwarden.url
 }
+
+output "cpus" {
+  value = module.vaultwarden.cpus
+}
+
+output "cputype" {
+  value = module.vaultwarden.cputype
+}
+
+output "memorymb" {
+  value = module.vaultwarden.memorymb
+}
+
+output "volume_size" {
+  value = module.vaultwarden.volume_size
+}
+
